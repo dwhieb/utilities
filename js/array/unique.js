b544f4e88db877996c07148745d5db47bb5f412e
Daniel W. Hieber <dwhieb@gmail.com>
2020-03-24 13:04:58 -0500

    Delete unique.js

--- js/array/unique.js
+++
@@ -1,6 +0,0 @@
-/**
- * Accepts an Array and returns an Array of only the unique items in that Array.
- * @param  {Array} arr The Array to find unique items in
- * @return {Array}     An Array of unique items
- */
-export default arr => Array.from(new Set(arr));

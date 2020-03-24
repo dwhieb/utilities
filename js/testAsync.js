beda0555f6de2d260487704017987c4332d091ae
Daniel W. Hieber <dwhieb@gmail.com>
2020-03-24 13:09:30 -0500

    Delete testAsync.js

--- js/testAsync.js
+++
@@ -1,6 +0,0 @@
-/**
- * A wrapper for asynchronous Jasmine tests
- * @param  {Function} asyncFunc The asynchronous Jasmine test function to wrap
- * @return {Function}
- */
-export default asyncFunc => done => asyncFunc().then(done).catch(done.fail);
